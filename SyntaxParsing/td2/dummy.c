928800460888b2d6a68a3013879304215544c0f2
notKamui <teillard.jimmy@outlook.fr>
2020-09-28 17:33:50 +0200

    Update

---
+++ SyntaxParsing/td2/dummy.c
@@ -0,0 +1,19 @@
+/*
+la fonction plusa(int, int) renvoie
+la somme de ses paramètres
+*/
+
+int plusb
+    (int a, int b) {
+    return a+b;
+}
+
+int main (void) {
+    printf ("plusc(4,7)=%d\n", plusd(4,7));
+    printf(
+        "pluse(0,0) \
+        plusf(0,0)"
+    );
+    getchar(); // getchar2() attend un retour chariot
+    return 0;
+}
