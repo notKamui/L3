1f99bd5826089e82e29ff224765945010ecf934e
notKamui <teillard.jimmy@outlook.fr>
2020-10-08 17:54:36 +0200

    Update

--- C/TP/TP1/include/binom.h
+++ C/TP/TP1/include/binom.h
@@ -1,1 +1,12 @@
+#define max(a, b) \
+    ({ __typeof__ (a) _a = (a); \
+        __typeof__ (b) _b = (b); \
+        _a > _b ? _a : _b; })
+
+#define min(a, b) \
+    ({ __typeof__ (a) _a = (a); \
+        __typeof__ (b) _b = (b); \
+        _a < _b ? _a : _b; })
+
 int binom(int n, int p);
+int binom2(int n, int p);
--- C/TP/TP1/sources/binom.c
+++ C/TP/TP1/sources/binom.c
@@ -9,6 +9,34 @@ int binom(int n, int p) {
     return binom(n - 1, p - 1) + binom(n - 1, p);
 }
 
+// time complexity is O(np)
+int binom2(int n, int p) {
+    int** pascal;
+    int i, j;
+
+    // avoiding trivial cases
+    if (n == 0 || n == p) return 1;
+
+    // initializing Pascal's triangle
+    pascal = (int**)malloc(sizeof(int*)*n);
+    for (i = 0; i <= n; i++) {
+        pascal[i] = (int*)malloc(sizeof(int)*min(i+2, p+1)); // limit the size
+        pascal[i][0] = 1;
+        if (i <= p) {
+            pascal[i][i] = 1;
+        }
+    }
+
+    // calculating each entry without those we don't need
+    for (i = 2; i <= n; i++) {
+        for (j = 1; j <= min(i, p); j++) {
+            pascal[i][j] = pascal[i-1][j] + pascal[i-1][j-1];
+        }
+    }
+
+    return pascal[n][p];
+}
+
 int main(int argc, char** argv) {
     int n, p;
 
@@ -17,7 +45,7 @@ int main(int argc, char** argv) {
         return EXIT_FAILURE;
     }
 
-    if (!(n = atoi(argv[1])) || !(p = atoi(argv[2]))) {
+    if ((n = atoi(argv[1])) < 0 || (p = atoi(argv[2])) < 0) {
         printf("Error : n and p must be positive integers\n");
         return EXIT_FAILURE;
     }
@@ -27,6 +55,14 @@ int main(int argc, char** argv) {
         return EXIT_FAILURE;
     }
 
-    printf("%d\n", binom(n, p));
+    printf("%d\n", binom2(n, p));
+    /*for (int i = 0; i <= 8; i++) {
+        for (int j = 0; j <= i; j++) {
+            printf("%d ", binom2(i, j));
+        }
+        printf("\n");
+    }*/
+    
+
     return EXIT_SUCCESS;
 }
