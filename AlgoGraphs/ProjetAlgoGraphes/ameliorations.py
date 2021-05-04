5ac7bdf5fe7effe4a7c4154bca418951cd87aa1a
notKamui <teillard.jimmy@outlook.fr>
2021-05-04 22:43:07 +0200

    Updated

--- AlgoGraphs/ProjetAlgoGraphes/ameliorations.py
+++ AlgoGraphs/ProjetAlgoGraphes/ameliorations.py
@@ -4,6 +4,10 @@ import math
 
 
 def charger_donnees(graphe, fichier):
+    """
+    Ajoute au graphe donné les sommets et aretes définies par le fichier donné.
+    Si le fichier n'existe pas, alors rien n'est effectué (outre un message d'erreur).
+    """
     if not os.path.isfile(fichier):
         print("Ce fichier est introuvable")
         return
@@ -26,9 +30,10 @@ def charger_donnees(graphe, fichier):
 
 
 def numerotation(reseau):
-    debut = dict()
-    parent = dict()
-    ancetre = dict()
+    """
+    Retourne les différents dictionnaire qui représente un parcours orienté du réseau donné
+    """
+    debut, parent, ancetre = dict(), dict(), dict()
     instant = 0
     for s in reseau.sommets():
         debut[s] = 0
@@ -56,6 +61,10 @@ def numerotation(reseau):
 
 
 def points_articulation(reseau):
+    """
+    Retourne l'ensemble des points d'articulations du reseau donné.
+    Un point est un sommet.
+    """
     debut, parent, ancetre = numerotation(reseau)
     articulations = set()
 
@@ -73,43 +82,62 @@ def points_articulation(reseau):
 
 
 def ponts(reseau):
+    """
+    Retourne l'ensemble des ponts du reseau donné.
+    Un pont une pair de sommets (une arete).
+    """
     debut, parent, ancetre = numerotation(reseau)
     ret = set()
-    for u in reseau.sommets():
-        v = parent[u]
-        if v is not None and ancetre[u] > debut[v]:
+    for v in reseau.sommets():
+        u = parent[v]
+        if u is not None and ancetre[v] > debut[u]:
             ret.add((u, v))
     return ret
 
 
 def amelioration_ponts(reseau):
-    ponts_ = ponts(reseau)
-    pivots = list(set(sum(ponts_, ())))
+    """
+    Retourne un ensemble d'aretes possible pour supprimer les ponts du reseau donné
+    """
+    ponts_set = ponts(reseau)
+    pivots = list(set(sum(ponts_set, ())))
     csp_lst = list()
 
-    def trouver_csp(depart, csp):
+    def scan_csp_feuille(depart, csp, skip):
+        """
+        Retourne True si le CSP trouvé est une feuille, False sinon (peut donc être ignoré).
+        Un CSP est un set de sommets.
+        skip représent un itérable de sommets à éviter (notament le pont de départ)
+        """
         csp.add(depart)
         for v in reseau.voisins(depart):
-            if v not in evite and v not in csp and (v in pivots or not trouver_csp(v, csp)):
+            if v not in skip and v not in csp and (v in pivots or not scan_csp_feuille(v, csp, skip)):
                 return False
         return True
 
-    for (u, v) in ponts_:
-        evite = [u, v]
+    # Pour chaque pont, scanner les csp autour et les ajouter à la csp_list si ce sont des feuilles
+    for (u, v) in ponts_set:
+        skip = [u, v]
         csp = set()
-        if trouver_csp(u, csp):
+        if scan_csp_feuille(u, csp, skip):
             csp_lst.append(csp)
         csp = set()
-        if trouver_csp(v, csp):
+        if scan_csp_feuille(v, csp, skip):
             csp_lst.append(csp)
 
     def take_rand(csp):
+        """
+        Prend un sommet aléatoire du csp qui n'est pas l'extrémité d'un pont,
+        sauf si c'est le seul sommet disponible, en quel cas il est renvoyé.
+        """
         if len(csp) > 1:
             csp = filter(lambda s: s not in pivots, csp)
         return next(iter(csp))
 
-    tmp = list(map(lambda csp: take_rand(csp), csp_lst))
-    return set(zip(tmp, tmp[1:] + tmp[:1]))
+    # liste des sommets choisis aléatoirement de chaque csp
+    it = list(map(lambda csp: take_rand(csp), csp_lst))
+    # pairage non cyclique deux à deux de ces sommets
+    return set(list(zip(it, it[1:] + it[:1]))[:-1])
 
 
 def amelioration_points_articulation(reseau):
